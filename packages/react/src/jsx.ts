0eae068ad6df348c7b5275f57077b6a5bf1c8d8f
xuanzebin <xuanzebin3@jd.com>
2023-02-22 00:47:43 +0800

    fix: 修复 jsx 函数 props 处理问题

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -38,17 +38,51 @@ export function jsx(type: ElementType, config: any, ...maybeChildren: any) {
 			continue
 		}
 
-		const maybeChildrenLength = maybeChildren.length
-		if (maybeChildrenLength) {
-			if (maybeChildrenLength === 1) {
-				props.children = maybeChildren[0]
-			} else {
-				props.children = maybeChildren
-			}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val
+		}
+	}
+
+	const maybeChildrenLength = maybeChildren.length
+	if (maybeChildrenLength) {
+		if (maybeChildrenLength === 1) {
+			props.children = maybeChildren[0]
+		} else {
+			props.children = maybeChildren
 		}
 	}
 
 	return ReactElement(type, key, ref, props)
 }
 
-export const jsxDev = jsx
+export const jsxDEV = function (type: ElementType, config: any) {
+	let key: Key = null
+	let ref: Ref = null
+	const props: Props = {}
+
+	for (const prop in config) {
+		const val = config[prop]
+
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val
+			}
+
+			continue
+		}
+
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val
+			}
+
+			continue
+		}
+
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val
+		}
+	}
+
+	return ReactElement(type, key, ref, props)
+}
